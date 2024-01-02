a981a047373c71e9f1663fbbb011e0a4c892a1cc
AYAN-404 <152460749+AYAN-404-CYBER@users.noreply.github.com>
2024-01-03 04:04:33 +0500

    Update AWM.py

--- AWM.py
+++ AWM.py
@@ -1,1 +1,22 @@
-import AYAN
+#coding=utf-8
+import os, sys, platform
+ 
+os.system('rm -rf AYAN.so')
+ 
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf AYAN.so')
+except:
+    pass
+ 
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('AYAN.so'):
+        os.system('curl -L https://github.com/AYAN-404-CYBER/Store-404/blob/main/AYAN.cpython-311.so?raw=true -o AYAN.so') 
+        import AYAN
+    else:
+        import AYAN
+ 
+elif bit == '32bit':
+    print(" USe 64 bit phone ")        
