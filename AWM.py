5340432596ce785500f781d3573041828aafea59
AYAN-404 <152460749+AYAN-404-CYBER@users.noreply.github.com>
2024-01-14 13:44:20 +0500

    Update AWM.py

--- AWM.py
+++ AWM.py
@@ -1,22 +1,29 @@
 #coding=utf-8
 import os, sys, platform
  
-os.system('rm -rf AYAN.so')
+os.system('rm -rf P64.so P32.so')
  
 try:
     if sys.argv[1]=='update':
-        os.system('rm -rf AYAN.so')
+        os.system('rm -rf P64.so P32.so')
 except:
     pass
  
  
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('AYAN.so'):
-        os.system('curl -L https://github.com/AYAN-404-CYBER/Store-404/blob/main/AYAN.cpython-311.so?raw=true -o AYAN.so') 
-        import AYAN
+    if not os.path.isfile('P64.so'):
+        os.system('curl -L https://github.com/AYAN-404-CYBER/Store-404/blob/main/P64.cpython-311.so?raw=true -o P64.so') 
+        import P64
     else:
-        import AYAN
+        import P64
  
 elif bit == '32bit':
-    print(" USe 64 bit phone ")        
+    if not os.path.isfile('P32.so'):
+        os.system('curl -L https://github.com/AYAN-404-CYBER/Store-404/blob/main/P32.cpython-311.so?raw=true -o P32.so') 
+        import P32
+    else:
+        import P32
+ 
+ 
+
